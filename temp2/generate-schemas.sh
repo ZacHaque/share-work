c42c12453abb12f56581139a23cc6105b9cc74bf
zac Haque <zahurul.ncs@gmail.com>
2025-06-20 18:17:45 +1000

    init

--- temp2/generate-schemas.sh
+++ temp2/generate-schemas.sh
@@ -2,6 +2,8 @@
 
 # List of schema names
 schema_names=("user" "order" "product")
+# schema_names=("user")
+
 
 # Overlay to use (dev or prod)
 overlay="${1:-dev}"  # default to 'dev' if not passed
@@ -12,8 +14,9 @@ if [[ "$overlay" != "dev" && "$overlay" != "prod" ]]; then
   exit 1
 fi
 
-base_dir="./kustomize/base"
-overlay_dir="./kustomize/overlays/${overlay}"
+base_dir="/Users/zac/my_workshop/kustomize/testing/base"
+overlay_dir="/Users/zac/my_workshop/kustomize/testing/overlays/${overlay}"
+output_dir="/Users/zac/my_workshop/kustomize/testing/output"
 
 for schema in "${schema_names[@]}"; do
   echo "🔧 Generating CR for schema: $schema using overlay: $overlay"
@@ -27,27 +30,37 @@ kind: Schema
 metadata:
   name: ${schema}-schema
 spec:
-  schema: |
-    {
-      "type": "record",
-      "name": "${schema^}",
-      "fields": [
-        { "name": "id", "type": "string" }
-      ]
-    }
   subject: ${schema}
   format: avro
   schemaRegistry:
     url: http://placeholder-schema-registry:8081
+    
 EOF
 
   # Create temporary kustomization.yaml pointing to the generated file
   echo "resources:
   - schema-${schema}.yaml" > "$base_dir/kustomization.yaml"
 
+
+  # Create a temp base CR file with the schema name injected
+  temp_overlay_cr="${overlay_dir}/patch-schema-endpoint.yaml"
+
+  cat > "$temp_overlay_cr" <<EOF
+apiVersion: platform.confluent.io/v1beta1
+kind: Schema
+metadata:
+  name: ${schema}-schema
+
+spec:
+  schemaRegistry:
+    url: http://dev-schema-registry.confluent:8081
+
+EOF
+
+
   # Dry-run to view final output
   echo "🚀 Dry-run output for schema: $schema"
-  kubectl apply --dry-run=client -k "$overlay_dir" -o yaml
+  kubectl apply --dry-run=client -k "$overlay_dir" -o yaml > ${output_dir}/${overlay}-${schema}-schema.yaml
 
   echo "---------------------------"
 
